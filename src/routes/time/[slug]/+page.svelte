72046d80a8441612e9efa9fc9d6b8efb9e2210e5
hoang.nguyen14 <hoang.nguyen14@sotatek.com>
2023-07-29 00:53:02 +0700

    fix:request error handling

--- src/routes/time/[slug]/+page.svelte
+++ src/routes/time/[slug]/+page.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
 	import Pagination from '$lib/components/Pagination.svelte';
-	/** @type {import('./$types').PageData} */
-	export let data: { content: string; chapter: number };
+	import type { PageData } from './$types';
+	export let data: PageData;
 </script>
 
-<div class="flex flex-col items-center px-3">
-	<Pagination chapter={data.chapter} className="mb-2" />
+<div class="flex flex-col items-center px-3 min-h-[95vh] justify-between">
+	<div class="flex flex-col items-center">
+		<Pagination chapter={data.chapter} className="mb-2" />
 
-	<h1 class="text-xl font-bold mb-1">Chương {data.chapter}</h1>
+		<h1 class="text-xl font-bold mb-1">Chương {data.chapter}</h1>
+	</div>
+
 	<p class="whitespace-pre-wrap">{data.content}</p>
-	<Pagination chapter={data.chapter} className="mt-2" />
+	<div class="flex flex-col items-center">
+		<h1 class="text-xl font-bold mb-1">Chương {data.chapter}</h1>
+		<Pagination chapter={data.chapter} className="mt-2" />
+	</div>
 </div>
