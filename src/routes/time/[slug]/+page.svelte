8febd1f62a501a2e3e412b77b633744c5f91bb5c
Hanumanman <=>
2023-07-29 14:18:48 +0700

    chore: add loading spinner

--- src/routes/time/[slug]/+page.svelte
+++ src/routes/time/[slug]/+page.svelte
@@ -1,19 +1,25 @@
 <script lang="ts">
+	import { navigating } from '$app/stores';
 	import Pagination from '$lib/components/Pagination.svelte';
+	import { Spinner } from 'flowbite-svelte';
 	import type { PageData } from './$types';
 	export let data: PageData;
 </script>
 
-<div class="flex flex-col items-center px-3 min-h-[95vh] justify-between">
-	<div class="flex flex-col items-center">
-		<Pagination chapter={data.chapter} className="mb-2" />
+<div class="flex flex-col items-center px-3 min-h-[95vh] justify-center">
+	{#if $navigating}
+		<Spinner />
+	{:else}
+		<div class="flex flex-col items-center">
+			<Pagination chapter={data.chapter} className="mb-2" />
 
-		<h1 class="text-xl font-bold mb-1">Chương {data.chapter}</h1>
-	</div>
+			<h1 class="text-xl font-bold mb-1">Chương {data.chapter}</h1>
+		</div>
 
-	<p class="whitespace-pre-wrap">{data.content}</p>
-	<div class="flex flex-col items-center">
-		<h1 class="text-xl font-bold mb-1">Chương {data.chapter}</h1>
-		<Pagination chapter={data.chapter} className="mt-2" />
-	</div>
+		<p class="whitespace-pre-wrap">{data.content}</p>
+		<div class="flex flex-col items-center">
+			<h1 class="text-xl font-bold mb-1">Chương {data.chapter}</h1>
+			<Pagination chapter={data.chapter} className="mt-2" />
+		</div>
+	{/if}
 </div>
