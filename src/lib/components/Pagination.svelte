8128d3276b18061bd5fb676b43c6b0a5d2e06cd9
Hanumanman <=>
2023-08-05 15:11:19 +0700

    refactor: change loading strategy between chapters

--- src/lib/components/Pagination.svelte
+++ src/lib/components/Pagination.svelte
@@ -7,38 +7,36 @@
 	export let chapter: number = 14;
 	let input = '';
 
-	const handleNextPage = () => {
-		chapter += 1;
-		goto(`/time/${chapter}`);
-	};
-	const handlePrevPage = () => {
-		chapter -= 1;
-		goto(`/time/${chapter}`);
-	};
+	// const handleNextPage = () => {
+	// 	chapter += 1;
+	// 	goto(`/time/${chapter}`);
+	// };
+	// const handlePrevPage = () => {
+	// 	chapter -= 1;
+	// 	goto(`/time/${chapter}`);
+	// };
 	const handleJump = () => {
 		goto(`/time/${input}`);
 	};
 </script>
 
 <div class={cn('flex space-x-2 items-center', className)}>
-	<Button
-		on:click={handlePrevPage}
-		class="flex justify-center items-center h-12 aspect-square"
-		aria-label="handlePrevPage"
-	>
-		<div class="w-3">
-			<svg
-				aria-hidden="true"
-				xmlns="http://www.w3.org/2000/svg"
-				fill="currentColor"
-				viewBox="0 0 10 16"
-			>
-				<path
-					d="M8.766.566A2 2 0 0 0 6.586 1L1 6.586a2 2 0 0 0 0 2.828L6.586 15A2 2 0 0 0 10 13.586V2.414A2 2 0 0 0 8.766.566Z"
-				/>
-			</svg>
-		</div>
-	</Button>
+	<a href={`/time/${chapter - 1}`} data-sveltekit-preload-data>
+		<Button class="flex justify-center items-center h-12 aspect-square" aria-label="handlePrevPage">
+			<div class="w-3">
+				<svg
+					aria-hidden="true"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="currentColor"
+					viewBox="0 0 10 16"
+				>
+					<path
+						d="M8.766.566A2 2 0 0 0 6.586 1L1 6.586a2 2 0 0 0 0 2.828L6.586 15A2 2 0 0 0 10 13.586V2.414A2 2 0 0 0 8.766.566Z"
+					/>
+				</svg>
+			</div>
+		</Button>
+	</a>
 	<form on:submit|preventDefault={handleJump}>
 		<div class="bg-myText p-1 rounded flex">
 			<Input
@@ -50,22 +48,20 @@
 			<Button class="ml-1 rounded-r rounded-l-none" type="submit">Go</Button>
 		</div>
 	</form>
-	<Button
-		on:click={handleNextPage}
-		class="flex justify-center items-center h-12 aspect-square"
-		aria-label="handleNextPage"
-	>
-		<div class="w-3">
-			<svg
-				aria-hidden="true"
-				xmlns="http://www.w3.org/2000/svg"
-				fill="currentColor"
-				viewBox="0 0 10 16"
-			>
-				<path
-					d="M3.414 1A2 2 0 0 0 0 2.414v11.172A2 2 0 0 0 3.414 15L9 9.414a2 2 0 0 0 0-2.828L3.414 1Z"
-				/>
-			</svg>
-		</div>
-	</Button>
+	<a href="/time/{chapter + 1}" data-sveltekit-preload-data>
+		<Button class="flex justify-center items-center h-12 aspect-square" aria-label="handleNextPage">
+			<div class="w-3">
+				<svg
+					aria-hidden="true"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="currentColor"
+					viewBox="0 0 10 16"
+				>
+					<path
+						d="M3.414 1A2 2 0 0 0 0 2.414v11.172A2 2 0 0 0 3.414 15L9 9.414a2 2 0 0 0 0-2.828L3.414 1Z"
+					/>
+				</svg>
+			</div>
+		</Button>
+	</a>
 </div>
