ba852e41579c8dd25b99794ebf31c2ff7e3a43ec
Hanumanman <hanumanman37@gmail.com>
2023-10-09 00:11:45 +0700

    chore: move to yarn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <div class="flex flex-col gap-4">
-	<a class=" hover:underline text-xl font-bold" href="/time/14">Nó lại là quang âm vãi lồn</a>
-	<a class=" hover:underline text-xl font-bold" href="/vozer/1">Nó lại là vozer vãi lồn</a>
+	<a class=" hover:underline text-xl font-bold" href="/time/14">No lai la quang am vl</a>
+	<a class=" hover:underline text-xl font-bold" href="/vozer/1">No lai la dat doc vl</a>
 </div>
